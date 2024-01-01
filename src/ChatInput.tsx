1056661393f7e96e7d1443d01e5f481f6a1246c0
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2024-01-01 17:26:25 +0100

    chat box edited

--- src/ChatInput.tsx
+++ src/ChatInput.tsx
@@ -1,11 +1,24 @@
 export const ChatInput = () => {
   return (
-    <div className="fixed bottom-0 w-full mb-4 px-10">
-      <input
-        type="text"
-        className="w-full px-3 py-2 border outline-none rounded-lg focus:border-indigo-500"
-        placeholder="Type Something..."
-      />
+    <div className="fixed bottom-0 w-full mb-4 px-3 md:px-10">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            className="w-full px-3 py-3 border outline-none rounded-lg focus:border-indigo-500 shadow-lg"
+            placeholder="Type Something..."
+            required
+          />
+          <div className="absolute right-4 md:right-11">
+            <button
+              className="bg-blue-700 text-white py-2 px-4 rounded-lg hover:bg-blue-600 active:bg-blue-700"
+              type="submit"
+            >
+              Send
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
